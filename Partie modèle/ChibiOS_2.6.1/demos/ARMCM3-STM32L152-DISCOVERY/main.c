91188e591430727a6502c381d421603efd38d480
SerialLooser <haroldducept@gmail.com>
2013-11-27 13:25:51 +0100

    Revert "Mise au propre du code."
    This reverts commit 5b473845ebd5dd319b8d00662f30785dc2b6b51f.

--- Partie modèle/ChibiOS_2.6.1/demos/ARMCM3-STM32L152-DISCOVERY/main.c
+++ Partie modèle/ChibiOS_2.6.1/demos/ARMCM3-STM32L152-DISCOVERY/main.c
@@ -23,6 +23,16 @@
 
 #define OSS 3 //Oversampling settings (taux d'échantillonage du BMP085 compris entre 0 (faible taux mais peu énergivore) et 3 (8 échantillons avant envois mais très énergivore))
 
+//static void pwmpcb(PWMDriver *pwmp);
+//static void adccb(ADCDriver *adcp, adcsample_t *buffer, size_t n);
+//static void spicb(SPIDriver *spip);
+
+///* Total number of channels to be sampled by a single ADC operation.*/
+//#define ADC_GRP1_NUM_CHANNELS	 2
+
+///* Depth of the conversion buffer, channels are sampled four times each.*/
+//#define ADC_GRP1_BUF_DEPTH			4
+
 /* Max number of mail in the box. */
 #define MAILBOX_SIZE						1
 
@@ -87,6 +97,70 @@ struct long_BMP085_reg
 	short md; //Registre 0xBE
 };
 
+//I2CConfig configI2C_1;
+//I2CDriver driverI2C_1;
+/*
+ * ADC samples buffer.
+ */
+//static adcsample_t samples[ADC_GRP1_NUM_CHANNELS * ADC_GRP1_BUF_DEPTH];
+
+/*
+ * ADC conversion group.
+ * Mode:				Linear buffer, 4 samples of 2 channels, SW triggered.
+ * Channels:		IN10	 (48 cycles sample time)
+ *							Sensor (192 cycles sample time)
+ */
+//static const ADCConversionGroup adcgrpcfg = {
+//	FALSE,
+//	ADC_GRP1_NUM_CHANNELS,
+//	adccb,
+//	NULL,
+//	/* HW dependent part.*/
+//	0,												/* CR1 */
+//	ADC_CR2_SWSTART,					/* CR2 */
+//	0,
+//	ADC_SMPR2_SMP_AN10(ADC_SAMPLE_48) | ADC_SMPR2_SMP_SENSOR(ADC_SAMPLE_192),
+//	0,
+//	ADC_SQR1_NUM_CH(ADC_GRP1_NUM_CHANNELS),
+//	0,
+//	0,
+//	0,
+//	ADC_SQR5_SQ2_N(ADC_CHANNEL_IN10) | ADC_SQR5_SQ1_N(ADC_CHANNEL_SENSOR)
+//};
+
+/*
+ * PWM configuration structure.
+ * Cyclic callback enabled, channels 1 and 2 enabled without callbacks,
+ * the active state is a logic one.
+ */
+//static PWMConfig pwmcfg = {
+//	10000,																		/* 10kHz PWM clock frequency.	 */
+//	10000,																		/* PWM period 1S (in ticks).		*/
+//	pwmpcb,
+//	{
+//		{PWM_OUTPUT_ACTIVE_HIGH, NULL},
+//		{PWM_OUTPUT_ACTIVE_HIGH, NULL},
+//		{PWM_OUTPUT_DISABLED, NULL},
+//		{PWM_OUTPUT_DISABLED, NULL}
+//	},
+//	/* HW dependent part.*/
+//	0,
+//	0
+//};
+
+/*
+ * SPI configuration structure.
+ * Maximum speed (12MHz), CPHA=0, CPOL=0, 16bits frames, MSb transmitted first.
+ * The slave select line is the pin GPIOA_SPI1NSS on the port GPIOA.
+ */
+//static const SPIConfig spicfg = {
+//	spicb,
+//	/* HW dependent part.*/
+//	GPIOB,
+//	12,
+//	SPI_CR1_DFF
+//};
+
 static const I2CConfig i2ccfg1 = 
 {
 	OPMODE_I2C,
@@ -99,7 +173,64 @@ static const I2CConfig i2ccfg2 = {
 		3000000, //100000, //
 		FAST_DUTY_CYCLE_16_9, //STD_DUTY_CYCLE, //
 };
+/*
+ * PWM cyclic callback.
+ * A new ADC conversion is started.
+ */
+//static void pwmpcb(PWMDriver *pwmp) {
 
+//	(void)pwmp;
+
+//	/* Starts an asynchronous ADC conversion operation, the conversion
+//		 will be executed in parallel to the current PWM cycle and will
+//		 terminate before the next PWM cycle.*/
+//	chSysLockFromIsr();
+//	adcStartConversionI(&ADCD1, &adcgrpcfg, samples, ADC_GRP1_BUF_DEPTH);
+//	chSysUnlockFromIsr();
+//}
+
+/*
+ * ADC end conversion callback.
+ * The PWM channels are reprogrammed using the latest ADC samples.
+ * The latest samples are transmitted into a single SPI transaction.
+ */
+//void adccb(ADCDriver *adcp, adcsample_t *buffer, size_t n) {
+
+//	(void) buffer; (void) n;
+//	/* Note, only in the ADC_COMPLETE state because the ADC driver fires an-
+//		 intermediate callback when the buffer is half full.*/
+//	if (adcp->state == ADC_COMPLETE) {
+//		adcsample_t avg_ch1, avg_ch2;
+
+//		/* Calculates the average values from the ADC samples.*/
+//		avg_ch1 = (samples[0] + samples[2] + samples[4] + samples[6]) / 4;
+//		avg_ch2 = (samples[1] + samples[3] + samples[5] + samples[7]) / 4;
+
+//		chSysLockFromIsr();
+
+//		/* Changes the channels pulse width, the change will be effective
+//			 starting from the next cycle.*/
+//		pwmEnableChannelI(&PWMD4, 0, PWM_FRACTION_TO_WIDTH(&PWMD4, 4096, avg_ch1));
+//		pwmEnableChannelI(&PWMD4, 1, PWM_FRACTION_TO_WIDTH(&PWMD4, 4096, avg_ch2));
+
+//		/* SPI slave selection and transmission start.*/
+//		spiSelectI(&SPID2);
+//		spiStartSendI(&SPID2, ADC_GRP1_NUM_CHANNELS * ADC_GRP1_BUF_DEPTH, samples);
+
+//		chSysUnlockFromIsr();
+//	}
+//}
+
+/*
+ * SPI end transfer callback.
+ */
+//static void spicb(SPIDriver *spip) {
+
+//	/* On transfer end just releases the slave select line.*/
+//	chSysLockFromIsr();
+//	spiUnselectI(spip);
+//	chSysUnlockFromIsr();
+//}
 
 static SerialConfig uartCfg=
 {
@@ -411,6 +542,22 @@ static msg_t ThreadAltitude(void *arg)
 	long pressure = 0;
 	long altitude = 0;
 	long pressure0 = 0;
+
+//	msg_t retour;
+
+//	uint8_t ac1[2] = {0}; //Registre 0xAA
+//	uint8_t ac2[2] = {0}; //Registre 0xAC
+//	uint8_t ac3[2] = {0}; //Registre 0xAE
+//	uint8_t ac4[2] = {0}; //Registre 0xB0
+//	uint8_t ac5[2] = {0}; //Registre 0xB2
+//	uint8_t ac6[2] = {0}; //Registre 0xB4
+
+//	uint8_t b1[2] = {0}; //Registre 0xB6
+//	uint8_t b2[2] = {0}; //Registre 0xB8
+
+//	uint8_t mb[2] = {0}; //Registre 0xBA
+//	uint8_t mc[2] = {0}; //Registre 0xBC
+//	uint8_t md[2] = {0}; //Registre 0xBE
 	
 	//Initialisation du BMP085
 	//récupération de ac1
@@ -489,6 +636,27 @@ static msg_t ThreadAltitude(void *arg)
 		altitude = 44330.75*(1-pow((double)pressure/pressure0, 0.19029))*100;
 		g_altitude = altitude;
 	}
+
+//		int i = 0;
+//		char addr = 0xEF; // define the I2C Address
+//		uint8_t rReg[3] = {0,0,0};
+//		char wReg[2] = {0,0};
+//		uint8_t cmd = 0x00;
+//		
+//		static i2cflags_t erreur = 0;
+//		char data[22];
+//		cmd = 0xAA;
+//		
+// 
+//		for (i = 0; i < 22; i++)
+//		{
+//			i2cAcquireBus(&I2CD1);	
+//			i2cMasterTransmitTimeout(&I2CD1, addr, &cmd, 1, rReg, 1, 10000000);
+//			erreur = i2cGetErrors(&I2CD1);
+//			i2cReleaseBus(&I2CD1);
+//			data[i] =	rReg[0];
+//			cmd += 2;
+//		}
 }
 
 static WORKING_AREA(waIntelligence, 128);
@@ -548,13 +716,53 @@ int main(void) {
 	i2cObjectInit(&I2CD2);
 	i2cStart(&I2CD2, &i2ccfg2);
 
+//	/*
+//	 * If the user button is pressed after the reset then the test suite is
+//	 * executed immediately before activating the various device drivers in
+//	 * order to not alter the benchmark scores.
+//	 */
+//	if (palReadPad(GPIOA, GPIOA_BUTTON))
+//		TestThread(&SD1);
+
+//	/*
+//	 * Initializes the SPI driver 2. The SPI2 signals are routed as follow:
+//	 * PB12 - NSS.
+//	 * PB13 - SCK.
+//	 * PB14 - MISO.
+//	 * PB15 - MOSI.
+//	 */
+//	spiStart(&SPID2, &spicfg);
+//	palSetPad(GPIOB, 12);
+//	palSetPadMode(GPIOB, 12, PAL_MODE_OUTPUT_PUSHPULL |
+//													 PAL_STM32_OSPEED_HIGHEST);					 /* NSS.		 */
+//	palSetPadMode(GPIOB, 13, PAL_MODE_ALTERNATE(5) |
+//													 PAL_STM32_OSPEED_HIGHEST);					 /* SCK.		 */
+//	palSetPadMode(GPIOB, 14, PAL_MODE_ALTERNATE(5));							/* MISO.		*/
+//	palSetPadMode(GPIOB, 15, PAL_MODE_ALTERNATE(5) |
+//													 PAL_STM32_OSPEED_HIGHEST);					 /* MOSI.		*/
+
+//	/*
+//	 * Initializes the ADC driver 1 and enable the thermal sensor.
+//	 * The pin PC0 on the port GPIOC is programmed as analog input.
+//	 */
+//	adcStart(&ADCD1, NULL);
+//	adcSTM32EnableTSVREFE();
+//	palSetPadMode(GPIOC, 0, PAL_MODE_INPUT_ANALOG);
+
+//	/*
+//	 * Initializes the PWM driver 4, routes the TIM4 outputs to the board LEDs.
+//	 */
+//	pwmStart(&PWMD4, &pwmcfg);
+//	palSetPadMode(GPIOB, GPIOB_LED4, PAL_MODE_ALTERNATE(2));
+//	palSetPadMode(GPIOB, GPIOB_LED3, PAL_MODE_ALTERNATE(2));
+
 	/*
 	 * Creates the example thread.
 	 */
 	chThdCreateStatic(waComRcv, sizeof(waComRcv), NORMALPRIO, ThreadComRcv, NULL);
 	chThdCreateStatic(waComSnd, sizeof(waComSnd), NORMALPRIO, ThreadComSnd, NULL);
 //	chThdCreateStatic(waLacet, sizeof(waLacet), NORMALPRIO, ThreadLacet, NULL);
-	chThdCreateStatic(waRoulisTangage, sizeof(waRoulisTangage), NORMALPRIO, ThreadRoulisTangage, NULL);
+	chThdCreateStatic(waRoulisTangage, sizeof(waRoulisTangage)+2000, NORMALPRIO, ThreadRoulisTangage, NULL);
 	chThdCreateStatic(waAltitude, sizeof(waAltitude)+1000, NORMALPRIO, ThreadAltitude, NULL);
 //	chThdCreateStatic(waIntelligence, sizeof(waIntelligence), NORMALPRIO, ThreadIntelligence, NULL);
 //	/*
@@ -565,6 +773,8 @@ int main(void) {
 
 //	 */
 	while (TRUE) {
+//		if (palReadPad(GPIOA, GPIOA_BUTTON))
+//			TestThread(&SD1);
 		chThdSleepMilliseconds(500);
 	}
 }
